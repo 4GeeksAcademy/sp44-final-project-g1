09b9577e5b27a21467da598f6527c87f8e507153
Andrea Betancourt <111385731+AndreaBetan@users.noreply.github.com>
2023-09-01 16:51:31 +0000

    Diagrama cargado

--- src/api/models.py
+++ src/api/models.py
@@ -4,9 +4,15 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(200), unique=True, nullable=False)
+    first_name = db.Column(db.String(50), unique=True, nullable=False)
+    last_name = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+
+    # Se requiere password??
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
 
     def __repr__(self):
         return f'<User {self.email}>'
