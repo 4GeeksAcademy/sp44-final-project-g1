ac5133fd30b8cd6a59505f7467460d65e4e36e16
Andrea Betancourt <111385731+AndreaBetan@users.noreply.github.com>
2023-09-08 11:51:56 +0000

    Endpoints with restrictions

--- src/api/routes.py
+++ src/api/routes.py
@@ -22,19 +22,29 @@ def handle_users():
         return response_body, 200
 
     if request.method == 'POST':
-        # 1. Obterner el contenido JSON de la solicitud POST entrante utilizando el objeto request. El método get_json()analiza y devuelve el contenido JSON de la solicitud
+        
+        # 1. Obtener el contenido JSON de la solicitud POST entrante utilizando el objeto request. El método get_json() analiza y devuelve el contenido JSON de la solicitud
         request_body = request.get_json()
-        # 2. Crear instancia de la clase User. Los valores para las propiedades email y password se toman del request_body
-        newuser = User(id_user_ghop=request_body["id_user_ghop"],
-                       fullname=request_body["fullname"])
-        # 3. Agrega el objeto newuser a la base de datos
-        db.session.add(newuser)
-        # 4.Confirmar los cambios
+        
+        # 2. Verificar si el usuario ya existe en la base de datos
+        existing_user = User.query.filter_by(id_user_ghop=request_body["id_user_ghop"]).first()
+        
+        if existing_user:
+            response_body = {"message": "User already exists."}
+            return jsonify(response_body), 400
+
+        # 3. Crear instancia de la clase User. Los valores para las propiedades id_user_ghop y fullname se toman del request_body
+        new_user = User(id_user_ghop=request_body["id_user_ghop"],
+                        fullname=request_body["fullname"])
+        
+        # 4. Agrega el objeto new_user a la base de datos
+        db.session.add(new_user)
+        # 5. Confirmar los cambios
         db.session.commit()
 
         response_body = {"message": "User registered successfully",
-                         "new user": request_body}
-        return response_body, 200
+                        "new user": request_body}
+        return jsonify(response_body), 200
 
 
 @api.route('/products', methods=['GET', 'POST'])
@@ -51,11 +61,19 @@ def handle_products():
     if request.method == 'POST':
         
         request_body = request.get_json()
+
+        existing_product = Product.query.filter_by(name=request_body["name"]).first()
+        
+        if existing_product:
+            response_body = {"message": "Product already exists."}
+            return jsonify(response_body), 400
+            
         product = Product(
             name=request_body["name"],
             pricing=request_body["pricing"],
             fk_category_id=request_body["fk_category_id"]
         )
+
         db.session.add(product)
         db.session.commit()
 
@@ -76,7 +94,17 @@ def handle_products_id(id):
 
         request_body = request.get_json()
         product = db.get_or_404(Product, id)
-        product.name = request_body['name']
+
+        # Verificar si el nuevo nombre ya existe en otros productos
+        new_name = request_body['name']
+        existing_product_with_name = Product.query.filter(Product.name == new_name, Product.id != id).first()
+        
+        if existing_product_with_name:
+            response_body = {"message": "The product name already exists."}
+            return jsonify(response_body), 400
+        
+        # Si el nombre es único, actualiza el producto
+        product.name = new_name
         product.pricing = request_body['pricing']
         product.fk_category_id = request_body['fk_category_id']
 
@@ -102,14 +130,21 @@ def handle_customers():
     if request.method == 'POST':
         
         request_body = request.get_json()
-        newcustomer = Customer(user_name=request_body["user_name"],
+        
+        existing_customer = Customer.query.filter_by(user_name=request_body['user_name']).first()
+
+        if existing_customer:
+            response_body = {"message": "Customer whit  already exists"}
+            return response_body, 400
+
+        new_customer = Customer(user_name=request_body["user_name"],
                                first_name=request_body["first_name"],
                                last_name=request_body["last_name"])
 
-        db.session.add(newcustomer)
+        db.session.add(new_customer)
         db.session.commit()
 
-        response_body = {"message": "User registered successfully",
+        response_body = {"message": "Customer registered successfully",
                          "new user": request_body}
         return response_body, 200
 
@@ -126,6 +161,14 @@ def handle_customer_id(id):
 
         request_body = request.get_json()
         customer = db.get_or_404(Customer, id)
+
+        new_username = request_body['user_name']
+        existing_customer_whit_username = Customer.query.filter_by(Custumor.user_name == new_username, Customer.id != id ).first()
+
+        if new_username:
+            response_body = {"message": "The customer with user name already exists."}
+            return response_body, 400
+
         customer.user_name = request_body['user_name']
         customer.first_name = request_body['first_name']
         customer.last_name = request_body['last_name']
@@ -150,6 +193,13 @@ def handle_categories():
 
     if request.method == 'POST':
         request_body = request.get_json()
+
+        existing_category = Category.query.filter_by(name=request_body["name"]).first()
+        
+        if existing_category:
+            response_body = {"message": "Category already exists."}
+            return jsonify(response_body), 400
+
         categories = Category(id=request_body["id"],
                               name=request_body["name"])
         db.session.add(categories)
@@ -172,15 +222,26 @@ def handle_categories_id(id):
     if request.method == 'PUT':
         request_body = request.get_json()
         category = db.get_or_404(Category, id)
-        category.id = request_body['id']
-        category.name = request_body['name']
-       
+
+        # Nuevo nombre de categoría
+        new_category_name = request_body['name']
+
+        # Verificar si existe una categoría con el nuevo nombre (distinta de la categoría actual)
+        existing_category_with_name = Category.query.filter(Category.name == new_category_name, Category.id != id).first()
+
+        if existing_category_with_name:
+            response_body = {"message": "The category with that name already exists."}
+            return response_body, 400
+
+        # Actualizar la categoría
+        category.name = new_category_name
         db.session.commit()
-       
-        response_body = {"message": 'Updating category',
-                         "status": 'ok',
-                         "result": request_body}
+
+        response_body = {"message": 'Updated category',
+                        "status": 'ok',
+                        "result": request_body}
         return response_body, 200
+
 
 
 @api.route('/bills', methods=['GET', 'POST'])
@@ -227,7 +288,6 @@ def handle_bills():
 
 
 """
-Pendiente realizar verificaciones de categorias y productos inexistentes
 Pendiente definir como realizar el login
 Enlazar el endpoint de Bill con el front para traer cada carrito de compras cerrado que el el que me alimentara la bill
 
